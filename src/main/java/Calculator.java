cbbfb0ea6ab403ef57268a8b3a94ce2d8784888a
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-04-27 13:33:12 -0500

    completed assertNotSame

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+    public static int sum(int int1, int int2){
+        return int1 + int2;
+    }
+
+    public static double tipCalculator(double price, double percentage) {
+        return price * (percentage/100);
+    }
 }
--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class MyFirstTest {
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+
+
+public class MyFirstTest {
+    @Before
+    public void setup(){
+        String codeup = "Codeup";
+    }
+
+    @Test
+    public void testAddition(){
+        int input1 = 6;
+        int input2 = 8;
+        int expectedResult = 14;
+
+        assertEquals(expectedResult, Calculator.sum(input1, input2));
+    }
+    @Test
+    public void testTipCalculator(){
+        double price = 10;
+        double percentage = 20;
+        int tip = 2;
+
+        assertEquals(tip, Calculator.tipCalculator(price,percentage), 0.01);
+    }
+
+//    @Test
+//    public void testCodeup(){
+//        String codeup = "Codeup";
+//        setup();
+//        assertEquals(codeup, "codeup");
+//    }
+
+    @Test
+    public void arrayTester(){
+        List<String> languages = new ArrayList<>();
+        List<String> moreLanguages = new ArrayList<>();
+        assertNotSame(languages, moreLanguages);
+    }
+
 }
